bde00eb677d437549e46c7962702498dad32baef
Danilo Woznica <danilowoz@gmail.com>
2024-02-06 22:11:08 +0000

    feat: Add import statement for NextResponse and return NextResponse with status 200

--- app/api/create-table/route.ts
+++ app/api/create-table/route.ts
@@ -1,5 +1,8 @@
 import { ensureDatabase } from "@/utils/data";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   await ensureDatabase();
+
+  return NextResponse.json({}, { status: 200 });
 }
