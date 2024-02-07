922e95f5101399352d5e3dd69d91bdcdd5537e23
Danilo Woznica <danilowoz@gmail.com>
2024-02-07 19:31:31 +0000

    fix: Fix missing import of Suspense component and format code to wrap Todo component with Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { Todo } from "@/components/Todo";
 import { getMovies } from "@/utils/data";
+import { Suspense } from "react";
 
 export default async function Home() {
   const movies = await getMovies();
 
-  return <Todo data={movies} />;
+  return (
+    <Suspense>
+      <Todo data={movies} />
+    </Suspense>
+  );
 }
